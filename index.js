f66dcf605493a63778bbe56236745a6013a8db8a
Adom <adomgbrhane@gmail.com>
2025-07-22 13:54:36 -0400

    Initial commit for nova demo app

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const PORT = 80;
+const MESSAGE = '🚀 Hello from Nova demo on AWS!';
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end(MESSAGE + '\n');
+}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
